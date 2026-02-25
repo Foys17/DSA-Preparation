c63689daf35d4ac356e5ce4abb37eb488e4623e2
Mehedy Hasan Foysal <mhfoysal17@gmail.com>
2026-02-25 15:24:04 +0600

    Add Palindrome Number solution in Python

---
+++ Problems/Math/Palindrome-Number.py
@@ -0,0 +1,19 @@
+"""
+Problem: Palindrome Number
+Reference: https://codeanddebug.in/blog/palindrome-number-program-in-python/
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        num = x
+        palindrome_number = 0
+        
+        while num > 0:
+            last_digit = num % 10
+            palindrome_number = (palindrome_number * 10) + last_digit
+            num //= 10
+        
+        return palindrome_number == x
